c8c7c19a875b0e52666da6915940d94787dbad81
JohnPAfr <john.aurry@gmail.com>
2019-07-27 10:03:15 +0200

    file with the const containing all the questions

---
+++ questions.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        "q" : "Capitale française",
+        "answer" : "Paris",
+        "a": "Lyon",
+        "b": "Bordeaux",
+        "c": "Paris",
+        "d": "Marseille"
+    },
+    {
+        "q" : "Département 75",
+        "answer" : "Paris",
+        "a": "Lyon",
+        "b": "Bordeaux",
+        "c": "Paris",
+        "d": "Marseille"
+    },
+    {
+        "q" : "Département 94",
+        "answer" : "Val de Marne",
+        "a": "Val d'oise",
+        "b": "Essone",
+        "c": "Yvelines",
+        "d": "Val de Marne"
+    },
+    {
+        "q" : "Dans quelle région se trouve l'Yonne",
+        "answer" : "Bourgogne",
+        "a": "Centre Val de Loire",
+        "b": "IDF",
+        "c": "Bourgogne Franche Comté",
+        "d": "Normandie"
+    }
+]
