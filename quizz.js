9db1ba311babdb89e91ab41a167b0700845a9f07
JohnPAfr <john.aurry@gmail.com>
2019-07-27 11:12:24 +0200

    creating an event for the start button to laucnh the game: 1st hide the start button then launch the game

--- quizz.js
+++ quizz.js
@@ -1,8 +1,30 @@
 let mode_choice = ["none", "multi", "single"];
-let mode = "";
+let mode = "none";
 
-let game = (mode) => {
+// SELECTORS
 
+let question = document.querySelector(".question");
+let message = document.querySelector(".message");   
+let start = document.querySelector(".start");
+
+
+let i = 0;
+
+
+start.addEventListener('click', () => {
+    setTimeout(() => {
+        start.style.display = "none";
+    }, 300)
+    setInterval(() => {
+        question.style.display = "block"
+        if (i >= questions.length)
+            return null;
+        question.textContent = questions[i].q;
+        i++;
+    }, 1000)
+})
+
+/* let game = (mode) => {
     switch (mode) {
         case 'none':
             mode_none();
@@ -19,7 +41,13 @@ let game = (mode) => {
 }
 
 function mode_none(questions) {
+    let question = document.querySelector(".question");
 
+    questions.array.forEach(q => {
+        setInterval(() => {
+            question.textContent = q.q;
+        }, 5000)
+    });
 }
 
 function mode_multi(questions) {
@@ -29,3 +57,5 @@ function mode_multi(questions) {
 function mode_single(questions) {
 
 }
+
+game(mode); */
