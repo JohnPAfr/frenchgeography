04de06a76b120acf545583e6dc3c82465ab92259
JohnPAfr <john.aurry@gmail.com>
2019-07-27 10:04:22 +0200

    first draw of the switch to choose the game mode, switch has unknown erros for now

--- quizz.js
+++ quizz.js
@@ -1,2 +1,31 @@
-let mode = ["none", "multi", "single"];
+let mode_choice = ["none", "multi", "single"];
+let mode = "";
 
+let game = (mode) => {
+
+    swith (mode) {
+        case 'none':
+            mode_none();
+            break;
+        case 'multi':
+            mode_multi();
+            break;
+        case 'single': 
+            mode_single();
+            break;
+        default:
+            return null;    
+    }
+}
+
+function mode_none(questions) {
+
+}
+
+function mode_multi(questions) {
+
+}
+
+function mode_single(questions) {
+
+}
